689b35dbbbb843ee54d6e3bbdc496f4be9c3401d
Federico Casenove <federico.casenove@gmail.com>
2026-05-30 11:06:05 +0200

    Aggiungi Service Worker per PWA standalone su Android

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
